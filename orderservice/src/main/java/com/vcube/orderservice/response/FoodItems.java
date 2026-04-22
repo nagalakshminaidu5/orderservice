2ba7279980cfff96134bf8ce975075c23ffc52e9
HP <HP@DESKTOP-SRCJT0F>
2026-04-22 12:31:56 -0700

    orderservice setvice

---
+++ orderservice/src/main/java/com/vcube/orderservice/response/FoodItems.java
@@ -0,0 +1,12 @@
+package com.vcube.orderservice.response;
+
+import lombok.Data;
+
+@Data
+public class FoodItems {
+
+	Integer id;
+	String name;
+	double price;
+	private Restaurant restaurant;
+}
---
+++ orderservice/src/main/java/com/vcube/orderservice/response/Restaurant.java
@@ -0,0 +1,11 @@
+package com.vcube.orderservice.response;
+
+import lombok.Data;
+
+@Data
+public class Restaurant {
+
+	Integer id;
+	String name;
+
+}
